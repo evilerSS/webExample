ae9df8ef37eed4e5c6b5e087b579370cc95ca594
YiWei.Zhao <eviler2018@gmail.com>
2020-02-11 21:23:35 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// import _ from "lodash";
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!');
+}
+
+function component() {
+  var element = document.createElement("pre");
+  element.innerHTML = 'Hello, Welcome!';
+
+  return element;
+}
+
+document.body.appendChild(component());
